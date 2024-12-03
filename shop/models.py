b8bf0e57704a12d59d66c41cb61943787b5e821e
codeAKstan <boystanismoses@gmail.com>
2024-12-03 16:37:08 +0100

    Rendered the views

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
     class Meta:
         ordering = ['name']
@@ -18,7 +23,7 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
-    Category = models.ForeignKey(Category, related_name='products',
+    category = models.ForeignKey(Category, related_name='products',
                                  on_delete=models.CASCADE)
     
     name = models.CharField(max_length=200)
@@ -29,6 +34,10 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
 
     class Meta:
         ordering = ['name']
--- shop/views.py
+++ shop/views.py
@@ -6,16 +6,19 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.all()
+    products = Product.objects.filter(available=True)
 
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
         products = products.filter(category=category)
+
     return render(request,
                   'shop/product/list.html',
-                  {'categories': categories,
-                   'products': products})
+                  {'category': category,
+                    'categories': categories,
+                   'products': products,
+                   })
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
